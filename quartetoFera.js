2a92ebf5690a354582158ce9d0b4ea31aceb0517
michelms314 <michel.marketer@gmail.com>
2020-02-03 17:11:16 -0300

    Junção do grupo fera! :)

---
+++ quartetoFera.js
@@ -0,0 +1,31 @@
+let grupo = [ 
+    {
+    nome: "Andressa",
+    passatempo:"assistir anime",
+    esporte:"caminhada",
+    memmrosDoGrupo: ["Andressa, AndersonN, AndersonR, Michel"]
+    },
+
+    {
+        nome: "Anderson",
+        passatempo: "Assistir",
+        esporte: "Natacão",
+        membros: ["Andressa, AndersonN, AndersonR, Michel"]
+    },
+
+    {
+        nome: "Michel",
+        passatempo: "Ouvir música, cantar, tocar violão, jogar, karaokê",
+        esporte: "passear com o cachorro",
+        membrosDoGrupo: ["Andressa, AndersonN, AndersonR, Michel"]
+    },
+
+    {
+        nome: "Nicácio",
+        passatempo: "Jogar Online",
+        Esporte: "Levantamento de Garfo",
+        MembrosDoGrupo: ["Andressa, AndersonN, AndersonR, Michel"]
+    }
+
+];
+
