084e3a71f17101ff9eaeefbbfb121ae396271391
anicacio <anderson@nicacio.com.br>
2020-02-03 17:00:45 -0300

    Adicionando arquivo nicacio.js

---
+++ nicacio.js
@@ -0,0 +1,8 @@
+let membros = [
+    {
+        nome: "Nicácio",
+        passatempo: "Jogar Online",
+        Esporte: "Levantamento de Garfo",
+        MembrosDoGrupo: ["Andressa", "Nicácio", "Ricardo", "Michel"]
+    }
+];
